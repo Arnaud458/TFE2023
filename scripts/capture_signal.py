bff8f517fe0b80a8c7b2f1d1328acc63120cb4c3
Arnaud458 <160670@umons.ac.be>
2024-06-02 18:14:39 +0200

    comments

--- scripts/capture_signal.py
+++ scripts/capture_signal.py
@@ -3,11 +3,16 @@ import time
 import numpy as np
 from rtlsdr import RtlSdr
 import hackrf
-from utils import cut_preamble, save_signal_old, read_file, write_file
+from utils import cut_preamble, save_signal, read_file, write_file
 from find_centers import SAMPLES_FOLDER
 
 
 SAMPLE_RATE = 2_000_000
+CAPTURE_DURATION = 2.5 # in seconds
+
+
+def compute_nb_samples():
+    return math.ceil(CAPTURE_DURATION * SAMPLE_RATE /16384)*16384
 
 
 def capture_signal_rtlsdr():
@@ -16,15 +21,10 @@ def capture_signal_rtlsdr():
     sdr.sample_rate = SAMPLE_RATE
     #sdr.bandwitdh = 250000
     sdr.center_freq = 867937500
-    sdr.gain = 5
+    sdr.gain = 2
 
-    # Start signal capture
-    capture_duration = 2.5  # in seconds
-
-    print(f"Capturing signal for {capture_duration} seconds...")
-    nb_samples = math.ceil(capture_duration * sdr.sample_rate /16384)*16384
-    #samples = sdr.read_samples(5046272)
-    samples = sdr.read_samples(nb_samples)
+    print(f"Capturing signal for {CAPTURE_DURATION} seconds...")
+    samples = sdr.read_samples(compute_nb_samples())
     
     sdr.close()
     samples = np.array(samples, dtype=np.complex64)
@@ -39,12 +39,8 @@ def capture_signal_hackrf():
     #hrf.lna_gain = 0
     #hrf.vga_gain = 0
 
-    # Start signal capture
-    capture_duration = 2.5  # in seconds
-
-    print(f"Capturing signal for {capture_duration} seconds...")
-    nb_samples = math.ceil(capture_duration * hrf.sample_rate /16384)*16384
-    samples = hrf.read_samples(nb_samples)
+    print(f"Capturing signal for {CAPTURE_DURATION} seconds...")
+    samples = hrf.read_samples(compute_nb_samples())
     
     hrf.close()
     samples = np.array(samples, dtype=np.complex64)
@@ -52,20 +48,19 @@ def capture_signal_hackrf():
 
 
 if __name__ == "__main__":
-    PREAMBLE_DURATION = 0.0245
+    PREAMBLE_DURATION = 0.098
 
-    for i in range(2):
+    for i in range(12):
         while read_file('tmp.txt')[-1] != '0':
             print('recepteur is waiting')
             time.sleep(0.2)
-
 
         SIGNAL = capture_signal_rtlsdr()
         #SIGNAL = capture_signal_hackrf()
         PREAMBLE = cut_preamble(SIGNAL, 0.03, int(PREAMBLE_DURATION*SAMPLE_RATE))
 
         if len(PREAMBLE) > 0:
-            save_signal_old(PREAMBLE,f'{SAMPLES_FOLDER}sample_{i+1}', np.complex64)
+            save_signal(PREAMBLE,f'{SAMPLES_FOLDER}sample_{i+1}', np.complex64)
             print(f'signal {i+1} saved')
         else:
             print(f"no signal found for {i+1}")
--- scripts/find_centers.py
+++ scripts/find_centers.py
@@ -3,38 +3,27 @@ from typing import Tuple, List
 import os
 import numpy as np
 import matplotlib.pyplot as plt
-from utils import compute_differential, load_signal
-from utils_display import using_datashader
+from utils import compute_differential, load_signal, DIFFERENTIAL_INTERVAL
+from utils_display import display_density
 from rn2483 import SPREADING_FACTOR
 
 RS = 125_000 / (2**SPREADING_FACTOR)
 JSON_FILENAME = "centers.json"
 DENSITY_THRESHOLD = 0.8
 
-MODULE = "RN1_norm"
+MODULE = "set36"
 SAMPLES_FOLDER = f'preambles/{MODULE}/'
 #SAMPLES_FOLDER = 'sample_data/test/'
 
 
-def not_close_to_zero(arr: np.array):
-    return np.sqrt(np.real(arr) ** 2 + np.imag(arr) ** 2) > 0.001
+def display_preamble(density, x_edges, y_edges, differential_i, differential_q, markedx, markedy):
+    """Display the signal in the complex plan, following the search of highest density points.
+        The area is divised in zones. The highest density zone is displayed in red, 
+        the zones that have density over the threshold are displayed in orange.
 
+        The points of the signal are displayed in blue (not depending of the zones)
+    """
 
-def display_density(i,q, filename):
-    fig, ax = plt.subplots()
-    #fig = plt.figure(figsize=(8, 6))
-    #ax = fig.add_subplot(1, 1, 1, projection='scatter_density')
-
-    using_datashader(ax, i, q)
-    #using_hist2d(ax, differential_i, differential_q)
-    #plt.scatter(differential_i, differential_q, s=1, c='red', alpha=0.5)
-    plt.xlabel('Differential In-phase')
-    plt.ylabel('Differential Quadrature')
-    plt.title('Differential I/Q Samples'+ filename)
-    #plt.grid(True)
-    plt.show()
-
-def display_preamble(density, x_edges, y_edges, differential_i, differential_q, markedx, markedy, filename):
     max_density_x_index, max_density_y_index = np.unravel_index(np.argmax(density), density.shape)
     max_density_x = (x_edges[max_density_x_index] + x_edges[max_density_x_index + 1]) / 2
     max_density_y = (y_edges[max_density_y_index] + y_edges[max_density_y_index + 1]) / 2
@@ -46,40 +35,37 @@ def display_preamble(density, x_edges, y_edges, differential_i, differential_q, 
     plt.scatter(markedx, markedy, s=5, c='orange', marker='o', label='Highest Density Points')
     plt.xlabel('Differential In-phase')
     plt.ylabel('Differential Quadrature')
-    plt.title('Differential Constellation Trace Figure '+ filename)
+    plt.title('Differential Constellation Trace Figure ')
     plt.legend()
     plt.grid(True)
     plt.show()
 
 
-def find_preamble_center(data: np.array, filename: str="") -> Tuple[int, int]:
-    n = 4096
-    delta_f = 0.5
-    differential_data = compute_differential(data, n)
-    #differential_data = data
-    #differential_data = differential_data[not_close_to_zero(differential_data)]
+def find_preamble_center(data: np.array) -> Tuple[int, int]:
+    # DCTF method
+    differential_data = compute_differential(data, DIFFERENTIAL_INTERVAL)
     differential_i = np.real(differential_data)
     differential_q = np.imag(differential_data)
 
     density, x_edges, y_edges = np.histogram2d(differential_i, differential_q, bins=300, density=True)
     highest_density_points_mask = density >= DENSITY_THRESHOLD * np.max(density)
-    # print("Number of Points in Highest Density Region:", np.sum(highest_density_points_mask))
-    # print("max_density", np.max(density))
+    highest_density_points = np.argwhere(highest_density_points_mask)
+    markedx = [(x_edges[p[0]] + x_edges[p[0] + 1]) / 2 for p in highest_density_points]
+    markedy = [(y_edges[p[1]] + y_edges[p[1] + 1]) / 2 for p in highest_density_points]
 
-    marked_points = np.argwhere(highest_density_points_mask)
-    markedx = [(x_edges[p[0]] + x_edges[p[0] + 1]) / 2 for p in marked_points]
-    markedy = [(y_edges[p[1]] + y_edges[p[1] + 1]) / 2 for p in marked_points]
+    # Uncomment the display the graph of the highest density points in the complex plan
+    # display_preamble(density, x_edges, y_edges, differential_i, differential_q, markedx, markedy)
 
-    #display_density(differential_i, differential_q, filename)
-    display_preamble(density, x_edges, y_edges, differential_i, differential_q, markedx, markedy, filename)
     return np.mean(markedx), np.mean(markedy)
 
 
 def find_preamble_centers(filepaths: List[str], module_data: dict) -> None:
     for filepath in filepaths:
         data = load_signal(filepath, dtype=np.complex64)
+        # Uncomment to display density graph of the preamble
+        # display_density(data, filepath)
+        center_x, center_y = find_preamble_center(data)
         filename = filepath.split('/')[-1]
-        center_x, center_y = find_preamble_center(data, filename)
         module_data[filename] = {
             "centerX": center_x,
             "centerY": center_y
@@ -88,7 +74,6 @@ def find_preamble_centers(filepaths: List[str], module_data: dict) -> None:
 
 if __name__ == "__main__":
 
-
     with open(JSON_FILENAME, 'r', encoding='utf8') as file_reader:
         centers_data = json.load(file_reader)
 
@@ -96,7 +81,6 @@ if __name__ == "__main__":
 
     if centers_data.get(MODULE) is None:
         centers_data[MODULE] = {}
-
 
     find_preamble_centers(
         [SAMPLES_FOLDER + filename for filename in files],
